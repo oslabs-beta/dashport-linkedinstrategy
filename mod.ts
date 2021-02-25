d66d1022e9fabc2408b4021ef3e87b1813085a96
Alvin Cheng <68521705+alcheng005@users.noreply.github.com>
2021-02-25 10:00:31 -0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 import Dashport from './lib/dashport.ts';
-import LinkedInStrategy from './linkedinStrategy';
+import LinkedInStrategy from './linkedinStrategy.ts';
 export default LinkedInStrategy;
